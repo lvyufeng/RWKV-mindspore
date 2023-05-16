d0d788c0a02fdc6988bdf87530b02f540e2b955f
lvyufeng <lvyufeng@cqu.edu.cn>
2023-05-16 15:47:52 +0800

    add backward test

--- tests/test_model.py
+++ tests/test_model.py
@@ -21,3 +21,24 @@ def test_forward(jit):
 
     outputs = forward(input_ids)
     print(outputs.shape)
+
+@pytest.mark.parametrize('jit', [True, False])
+def test_backward(jit):
+    config = GPTConfig(1000, 512, model_type='RWKV', n_layer=2, n_embd=128)
+    model = GPT(config)
+    
+    input_ids = Tensor(np.random.randint(0, 1000, (2, 32)), mindspore.int32)
+    def forward(input_ids, labels):
+        loss = model(input_ids, labels)
+        return loss
+
+    grad_fn = mindspore.value_and_grad(forward, None, model.trainable_params())
+
+    def train_step(input_ids, labels):
+        loss, grads = grad_fn(input_ids, labels)
+        return loss
+    if jit:
+        train_step = ms_jit(train_step)
+
+    loss = train_step(input_ids[:-1], input_ids[1:])
+    print(loss)
